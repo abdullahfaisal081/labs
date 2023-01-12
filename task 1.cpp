7f053493b02d2610f15b9e01f686e9b65003e35b
abdullahfaisal081 <122567916+abdullahfaisal081@users.noreply.github.com>
2023-01-13 01:29:20 +0500

    Add files via upload

---
+++ task 1.cpp
@@ -0,0 +1,25 @@
+// task 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char x;
+    cout << "Enter a character (Must consist of CAPITAL, SMALL letter or DIGIT) :";
+    cin >> x;
+    int a = int(x);
+    cout << "The ASCII value is :" << a << "\n";
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
